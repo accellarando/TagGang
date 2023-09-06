13ed5841fa96a34680b9f3803b72987e0f9c913e
Ella Moss <ella.moss@utah.edu>
2023-09-05 21:48:46 -0600

    oops now there are two resources >:( fixed it

--- docs/scripts/navbar.js
+++ docs/scripts/navbar.js
@@ -10,9 +10,6 @@ const navElement = `
 			<li class="nav-item">
 				<a class="nav-link" href="/TagGang/schedule.html">Schedule</a>
 			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="/TagGang/notes/resources.html">Resources</a>
-			</li>
 			<li class="nav-item dropdown">
 				<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Hardware</a>
 				<div class="dropdown-menu">
