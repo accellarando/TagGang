6811bd79ca656495238c776aa2abb6a8a38e7b56
Dana? <33265598+danaescandor@users.noreply.github.com>
2023-11-12 20:43:22 -0700

    trying drawing widget to simulate selector and grid space

--- software/selector_v1.c
+++ software/selector_v1.c
@@ -1,36 +1,38 @@
-// Template main.c, example-3.c (w/out XML)
-
 #include <gtk/gtk.h>
 
 // Global vars
-GtkWidget *selector;
 GtkWidget *fixed; // Store fixed container globally
 int gridSize = 32; // Adjust accordingly
-int selectorSize = 32; // Adjust accordingly
 int maxX, maxY;
+int selectorX = 0, selectorY = 0;
+gboolean selectorHighlighted = FALSE;
+
+gboolean draw_selector(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+    if (selectorHighlighted) {
+        cairo_set_source_rgba(cr, 0.0, 0.8, 0.0, 0.5); // Green with 50% transparency
+        cairo_rectangle(cr, selectorX, selectorY, gridSize, gridSize);
+        cairo_fill(cr);
+    }
+
+    return FALSE;
+}
 
 gboolean key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
-    GdkRectangle allocation;
-    gtk_widget_get_allocation(selector, &allocation);
-    int x = allocation.x;
-    int y = allocation.y;
-
-    switch (event->keyval) // From struct GdkEventKey
-    {
+    switch (event->keyval) {
         case GDK_KEY_Up:
-            y -= gridSize;
+            selectorY -= gridSize;
             g_print("UP\n");
             break;
         case GDK_KEY_Down:
-            y += gridSize;
+            selectorY += gridSize;
             g_print("DOWN\n");
             break;
         case GDK_KEY_Left:
-            x -= gridSize;
+            selectorX -= gridSize;
             g_print("LEFT\n");
             break;
         case GDK_KEY_Right:
-            x += gridSize;
+            selectorX += gridSize;
             g_print("RIGHT\n");
             break;
         default:
@@ -38,23 +40,22 @@ gboolean key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer user_da
     }
 
     // Clamps the selector within the boundary
-    x = MAX(0, MIN(x, maxX - selectorSize));
-    y = MAX(0, MIN(y, maxY - selectorSize));
+    selectorX = MAX(0, MIN(selectorX, maxX - gridSize));
+    selectorY = MAX(0, MIN(selectorY, maxY - gridSize));
 
     // Snap to the grid
-    x = (x / gridSize) * gridSize;
-    y = (y / gridSize) * gridSize;
+    selectorX = (selectorX / gridSize) * gridSize;
+    selectorY = (selectorY / gridSize) * gridSize;
 
-    // Set new position
-    if (fixed != NULL) {
-        gtk_fixed_move(GTK_FIXED(fixed), selector, x, y);
-    }
+    // Redraw the fixed container
+    gtk_widget_queue_draw(GTK_WIDGET(fixed));
 
     return TRUE;
 }
 
 int main(int argc, char *argv[]) {
     GtkWidget *window;
+    GtkWidget *drawing_area;
     GError *error = NULL;
 
     gtk_init(&argc, &argv);
@@ -66,14 +67,18 @@ int main(int argc, char *argv[]) {
     fixed = gtk_fixed_new();
     gtk_container_add(GTK_CONTAINER(window), fixed);
 
-    selector = gtk_button_new_with_label("Selector"); // TODO: change into grid
-    gtk_fixed_put(GTK_FIXED(fixed), selector, 0, 0);
+    drawing_area = gtk_drawing_area_new();
+    gtk_fixed_put(GTK_FIXED(fixed), drawing_area, 0, 0);
+    gtk_widget_set_size_request(drawing_area, 400, 400);
 
     // Connect the key press event
     g_signal_connect(G_OBJECT(window), "key-press-event", G_CALLBACK(key_press_event), NULL);
 
     // Connect the destroy event to quit the application
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    // Connect the draw event to the drawing area
+    g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(draw_selector), NULL);
 
     // Get the size of the fixed container
     gtk_widget_get_size_request(fixed, &maxX, &maxY);
