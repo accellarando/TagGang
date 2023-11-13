62ae06ab981c3b7ebc5c44f3f5bfafa33cfdc2b9
Dana? <33265598+danaescandor@users.noreply.github.com>
2023-11-12 22:26:49 -0700

    correct version to test

--- software/test.c
+++ software/test.c
@@ -2,73 +2,65 @@
 
 #define GRID_SIZE 50
 
-GtkWidget *window;
-GtkWidget *fixed;
-GtkWidget *highlightBox;
+static GtkWidget *drawing_area;
+static int box_x = 0;
+static int box_y = 0;
 
-int boxX = 0;
-int boxY = 0;
-
-void moveBox(int x, int y) {
-    boxX = x;
-    boxY = y;
-
-    gtk_fixed_move(GTK_FIXED(fixed), highlightBox, boxX, boxY);
+static void draw_highlight_box(cairo_t *cr, int x, int y) {
+    cairo_set_source_rgb(cr, 0, 1, 0); // Set color to green
+    cairo_rectangle(cr, x, y, GRID_SIZE, GRID_SIZE);
+    cairo_fill(cr);
 }
 
-static gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer data) {
+    draw_highlight_box(cr, box_x, box_y);
+    return FALSE;
+}
+
+static void move_box(int dx, int dy) {
+    box_x += dx * GRID_SIZE;
+    box_y += dy * GRID_SIZE;
+
+    gtk_widget_queue_draw(drawing_area);
+}
+
+static void on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
     switch (event->keyval) {
-        case GDK_KEY_Left:
-            moveBox(boxX - GRID_SIZE, boxY);
-            break;
-
-        case GDK_KEY_Right:
-            moveBox(boxX + GRID_SIZE, boxY);
-            break;
-
         case GDK_KEY_Up:
-            moveBox(boxX, boxY - GRID_SIZE);
+            move_box(0, -1);
             break;
-
         case GDK_KEY_Down:
-            moveBox(boxX, boxY + GRID_SIZE);
+            move_box(0, 1);
             break;
-
+        case GDK_KEY_Left:
+            move_box(-1, 0);
+            break;
+        case GDK_KEY_Right:
+            move_box(1, 0);
+            break;
         default:
             break;
     }
-
-    return FALSE;
 }
 
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
-    // Create the main window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Grid Snapping");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
-    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Snapping Box");
+    gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // Create a fixed container to hold other widgets
-    fixed = gtk_fixed_new();
-    gtk_container_add(GTK_CONTAINER(window), fixed);
+    drawing_area = gtk_drawing_area_new();
+    gtk_container_add(GTK_CONTAINER(window), drawing_area);
 
-    // Create the green highlight box
-    highlightBox = gtk_event_box_new();
-    GdkColor color;
-    gdk_color_parse("green", &color);
-    gtk_widget_modify_bg(highlightBox, GTK_STATE_NORMAL, &color);
-    gtk_widget_set_size_request(highlightBox, GRID_SIZE, GRID_SIZE);
-    gtk_fixed_put(GTK_FIXED(fixed), highlightBox, boxX, boxY);
+    g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(on_draw_event), NULL);
+    g_signal_connect(window, "key-press-event", G_CALLBACK(on_key_press), NULL);
 
-    // Connect the key press event
-    g_signal_connect(G_OBJECT(window), "key-press-event", G_CALLBACK(on_key_press), NULL);
+    gtk_widget_set_events(window, GDK_KEY_PRESS_MASK);
 
-    // Show all widgets
     gtk_widget_show_all(window);
 
-    // Start the GTK main loop
     gtk_main();
 
     return 0;
