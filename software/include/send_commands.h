e1d21ffdc111032a6e303bfe714aab1df1d8baba
Dana? <33265598+danaescandor@users.noreply.github.com>
2023-11-25 18:06:11 -0700

    pseudo code for serial commands

---
+++ software/include/send_commands.h
@@ -0,0 +1,19 @@
+#ifndef SEND_COMMANDS_H
+#define SEND_COMMANDS_H
+
+#include <gtk/gtk.h>
+#include <math.h>
+#include <main.h>
+
+
+/* Global variables */
+#define GRID_SIZE 50
+#define SNAP_INTERVAL 0.5
+#define WINDOW_WIDTH 800 // Increased window width to accommodate the image display area
+#define WINDOW_HEIGHT 500
+
+void activate_plotter(GObject* self,
+		GParamSpec* pspec,
+  gpointer user_data);
+
+#endif
--- software/send_commands.c
+++ software/send_commands.c
@@ -6,12 +6,28 @@
  * Once EOF is reached, change title back to the TITLE_CANVAS.
  */
 #include <send_commands.h>
+#include <stdio.h>
 
 /**
  * Sets up the GUI for this stage, then starts the file reading/sending process.
  */
 void activate_plotter(GtkApplication *app, void* hi, gpointer data){
 	// Set up GUI with some placeholder text
-	
+	// TODO: put a loading bar here that lets you keep track of progress. For now, just put some placeholder text.
+	GtkWidget *label = gtk_label_new("Sending over serial!");
+    gtk_container_add(GTK_CONTAINER(window), label);
+    gtk_widget_show_all(window);
+
+	finish_sending_stage();
 	// Read file line by line, send over serial terminal
+}
+
+static void finish_sending_stage() {
+	// Clean up widgets
+	gtk_widget_destroy(drawing_area);
+	gtk_widget_destroy(image_display_area);
+	gtk_widget_destroy(vbox);
+
+	// Change window title
+	gtk_window_set_title(GTK_WINDOW(window), TITLE_GCODER);
 }
