45ee80385e5c51ba84bdbafb491d9aed82f2a62b
Dana Escandor <escandordana@gmail.com>
2023-12-02 16:57:00 -0700

    debug files to hold old serial code to debug

---
+++ software/debug/serialsend-py-debug.py
@@ -0,0 +1,16 @@
+ import serial
+ import time
+
+ arduino = serial.Serial(port='/dev/ttyACM0', baudrate= 9600, timeout= 1)
+
+ def write_read(x):
+     arduino.write(bytes(x, 'utf-8'))
+     time.sleep(0.05)
+     data = arduino.readline()
+     return data
+
+
+ while True:
+     num = input("Enter a num: ")
+     value = write_read(num)
+     print(value.decode().strip())
