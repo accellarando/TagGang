4d48a5a1b8a400b88acfa87f8927063c79a6db20
Dana Escandor <escandordana@gmail.com>
2023-12-02 19:06:33 -0700

    debug lines, is it sending properly?

--- software/debug/sendcmd-debug.c
+++ software/debug/sendcmd-debug.c
@@ -6,38 +6,49 @@
  * Once EOF is reached, change title back to the TITLE_CANVAS.
  */
 //#include <send_commands.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 512
+#define BUFFER_SIZE 512 //256
+#define GCODE_FILE_PATH "/home/ella/Desktop/TagGang/software/debug/gtext.gcode"
+#define PYTHON_SCRIPT_PATH "/home/ella/Desktop/TagGang/software/serialsend.py"
 
 static int send_command(char* cmd){
-	char* python = "python serialsend.py %s";
+	// constructs the command to call py script
+	char* python = "python3 %s \"%s\"";
 	char buffer[BUFFER_SIZE + strlen(python)];
-	sprintf(buffer, python, cmd);
-	int status = system(cmd);
-	return status;
+	sprintf(buffer, python, PYTHON_SCRIPT_PATH, cmd);
+	system(cmd);
+	//int status = system(cmd);
+	//return status;
 }
 
 static void parse_gcode(){
-	FILE *gcode_file = fopen("/home/ella/Desktop/TagGang/software/debug/gtext.gcode", "r");
+	FILE *gcode_file = fopen(GCODE_FILE_PATH, "r");
 	if(gcode_file == NULL){
 		printf("Error opening gcode file!\n");
+		//exit(EXIT_FAILURE);
 	}
 
-	char line[BUFFER_SIZE];
+	char line[256];
 	while(fgets(line, sizeof(line), gcode_file) != NULL){
-		int err = send_command(line);
-		if(err != 0)
-			printf("Error from plotter: %d\n", err);
+		line[strcspn(line, "\n")] = '\0'; // remove newline char
+		printf("%sEND_", line);
+		//send_command(line);
+		//int err = send_command(line);
+		//if(err != 0)
+			//printf("Error from plotter: %d\n", err);
 	}
 
 	fclose(gcode_file);
 }
 
-parse_gcode();
+int main() {
+	parse_gcode();
+	return 0;
+}
 
 
 
