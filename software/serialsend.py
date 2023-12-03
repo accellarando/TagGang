9aaec2663aff280085264f186c8e9fc0b1ce78e5
Dana Escandor <escandordana@gmail.com>
2023-12-02 23:08:18 -0700

    added comments

--- software/serialsend.py
+++ software/serialsend.py
@@ -1,7 +1,19 @@
+"""
+serialsend:
+ 
+Python script for sending gcode commands to Arduino over serial connection.
+Utilizes pyserial library for easier integration (see serialsend.c for C version).
+
+@author Dana Escandor
+"""
 import sys
 import serial
 import time
 
+"""
+parses command-line argument sent from send_commands.c to call send_to_arduino
+with provided gcode command
+"""
 def parse_args():
     if len(sys.argv) < 2:
         return -1
@@ -10,22 +22,26 @@ def parse_args():
     #returned = send_to_arduino(sys.argv[1:])
     #print(f"returned: {returned}")
 
+"""
+parses command-line argument sent from send_commands.c to be args for send_to_arduino
+"""
 def send_to_arduino(gcode_command):
-	#print("IN python: send_to_arduino");
-	ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=1)
-	gcode_command = gcode_command[0].strip("[]'")
-	data = (f"{gcode_command}\n").encode()
-	#print(data);
-	retval = ser.write(data)
+	ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=1) # set up serial connection
 	
-	response = ser.readline().decode().strip()
+	gcode_command = gcode_command[0].strip("[]'") # remove array notation
+	data = (f"{gcode_command}\n").encode() # encode bytes for serial to read properly
+	#print(data);
+	retval = ser.write(data) # send data to via serial Arduino
+	
+	response = ser.readline().decode().strip() # decode bytes received back from serial
 	print(f"Arduino Response: {response}")
 		
 	ser.close()
 	
-	# printing data:
+	# (debugging) printing data:
 	# b"['G1 L217.503103 R1282.552845\\n']" >> before removing newline in parse_gcode
 	# b"['G1 L217.503103 R1282.552845']" >> before stripping
 	# b'G1 L217.503103 R1282.552845\n' >> CORRECT
 
+# initiate script with parse_args
 parse_args();
